b4ba23e2a670053f5d44bc17fc51eece3225798b
Chau Tran <cctran96@gmail.com>
2021-06-21 14:05:57 -0700

    bug fixes + changed class component to functional component

--- src/auth/Signup.js
+++ src/auth/Signup.js
@@ -1,68 +1,60 @@
-import React, { Component } from 'react'
-import { withRouter } from 'react-router'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
 const userUrl = "http://localhost:3000/api/v1/users"
 const allUsers = "http://localhost:3000/api/v1/usernames"
 
-class Signup extends Component {
-    state = {
-        username: "",
-        password: "",
-        confirm: "",
-        errors: []
+const Signup = ({setUser}) => {
+    const [userForm, setUserForm] = useState({username: "", password: "", confirm: ""})
+    const [errors, setErrors] = useState([])
+    const history = useHistory()
+
+
+    const handleChange = e => {
+        setUserForm({...userForm, [e.target.name]: e.target.value})
     }
 
-    handleChange = e => {
-        this.setState({[e.target.name]: e.target.value})
-    }
-
-    sendAuthInfo = () => {
+    const sendAuthInfo = () => {
         const config = {
             method: "POST",
             headers: {
                 "Content-Type" : "application/json",
                 Accept : "application/json"
             },
-            body: JSON.stringify({username: this.state.username, password: this.state.password})
+            body: JSON.stringify({username: userForm.username, password: userForm.password})
         }
         fetch(userUrl, config).then(r => r.json()).then(data => {
-            this.props.setUser(data.user)
+            setUser(data.user)
             localStorage.setItem("jwt", data.jwt)
-            this.props.history.replace("/")
+            history.replace("/")
         })
     }
 
-    handleSignup= e => {
+    const handleSignup = e => {
         e.preventDefault()
-        const s = this.state
         fetch(allUsers).then(r => r.json()).then(data => {
-            const errors = []
-            data.usernames.includes(s.username) ? errors.push("The username you have entered is already taken") : console.log()
-            s.password !== s.confirm ? errors.push("The password you have entered does not match") : console.log()
-            s.password.length < 6 || s.confirm.length < 6 ? errors.push("Your password must be at least 6 characters") : console.log()
-            this.setState({errors: errors}, () => {
-                !s.errors.length ? this.sendAuthInfo() : console.log()
-            })
+            const newErrors = []
+            data.usernames.includes(userForm.username) ? newErrors.push("The username you have entered is already taken") : console.log()
+            userForm.password !== userForm.confirm ? newErrors.push("The password you have entered does not match") : console.log()
+            userForm.password.length < 6 || userForm.confirm.length < 6 ? newErrors.push("Your password must be at least 6 characters") : console.log()
+            setErrors(newErrors)
+            !newErrors.length ? sendAuthInfo() : console.log()
         })
-        
     }
 
-    render() {
-        const s = this.state
-        return (
-            <div>
-                <form onSubmit={e => this.handleSignup(e)}>
-                    <input onChange={e => this.handleChange(e)} value={s.username} name="username" placeholder="Enter your username..." required/>
-                    <input onChange={e => this.handleChange(e)} value={s.password} name="password" placeholder="Enter your passwprd..."type="password" required/>
-                    <input onChange={e => this.handleChange(e)} value={s.confirm} name="confirm" placeholder="Renter your password..."type="password" required/>
-                    <input type="submit"/>
-                </form>
-                <ul>
-                    {s.errors.map(error => <li>{error}</li>)}
-                </ul>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <form onSubmit={e => handleSignup(e)}>
+                <input onChange={e => handleChange(e)} value={userForm.username} name="username" placeholder="Enter your username..." required/>
+                <input onChange={e => handleChange(e)} value={userForm.password} name="password" placeholder="Enter your password..."type="password" required/>
+                <input onChange={e => handleChange(e)} value={userForm.confirm} name="confirm" placeholder="Renter your password..."type="password" required/>                    
+                <input type="submit"/>
+            </form>
+            <ul>
+                {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+            </ul>
+        </div>
+    )
 }
 
-export default withRouter(Signup)
+export default Signup
