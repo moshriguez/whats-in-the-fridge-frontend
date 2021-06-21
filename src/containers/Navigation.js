d98c3483259ed7e7c844ac4095c1078877d2d094
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-06-21 16:23:44 -0700

    an update

--- src/containers/Navigation.js
+++ src/containers/Navigation.js
@@ -9,7 +9,7 @@ const Navigation = () => {
             <Navbar.Brand>What's in the Fridge?</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse className="justify-content-end">
-            <Nav variant="pills" defaultActiveKey="/home">
+            <Nav variant="tabs" defaultActiveKey="/home">
                 <Nav.Link eventKey="home" ><NavLink to="/">Home</NavLink></Nav.Link>
                 <Nav.Link eventKey="fridge" ><NavLink to="/fridge">My Fridge</NavLink></Nav.Link>
                 <Nav.Link eventKey="login" href="/login"><NavLink to="/login">Login</NavLink></Nav.Link>
